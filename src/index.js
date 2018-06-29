a2d3f2f636083cc2b53bf65ad2a1b5d95bc19edc
yuta-ushijima <register@yuta-u.com>
2018-06-29 22:42:37 +0900

    Render Targets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // 新しいコンポーネントを作成する
-const App = function () {
+const App = () => {
   return <div>Hi!</div>;
 }
 // このコンポーネントはHTMLを生成し、DOMによって描画する
+ReactDOM.render(<App />, document.querySelector('.container'));
