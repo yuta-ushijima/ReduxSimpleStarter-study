121ba1ca15cb31344fe144158d94bbb7d861e5bc
yuta-ushijima <register@yuta-u.com>
2018-07-01 22:11:52 +0900

    検索バーの実装(SearchBarクラスコンポーネントを定義)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBvhL2W0I8uA_ZvgVLvFrTnCvPinOsBzJU';
 
 // 新しいコンポーネントを作成する
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 // このコンポーネントはHTMLを生成し、DOMによって描画する
 ReactDOM.render(<App />, document.querySelector('.container'));
